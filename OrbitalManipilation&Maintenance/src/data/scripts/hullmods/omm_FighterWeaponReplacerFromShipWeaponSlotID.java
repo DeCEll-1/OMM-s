34488a78c0ec1cf73ab617e7b89548c926c99fbc
Arda Eren <70160402+DeCEll-1@users.noreply.github.com>
2023-05-28 16:33:49 +0300

    x

--- OrbitalManipilation&Maintenance/src/data/scripts/hullmods/omm_FighterWeaponReplacerFromShipWeaponSlotID.java
+++ OrbitalManipilation&Maintenance/src/data/scripts/hullmods/omm_FighterWeaponReplacerFromShipWeaponSlotID.java
@@ -4,46 +4,72 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.loading.WeaponSlotAPI;
 import com.fs.starfarer.api.loading.WeaponSpecAPI;
+import com.fs.starfarer.combat.entities.Ship;
 import org.apache.log4j.Logger;
 
 public class omm_FighterWeaponReplacerFromShipWeaponSlotID extends BaseHullMod {
 
-    private static final Logger log = Global.getLogger(omm_FighterWeaponReplacerFromShipWeaponSlotID.class);
+    public boolean DoOnce = true;
+
 
     @Override
     public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
-
-        for (WeaponAPI weapon : ship.getAllWeapons()) {//gets weapons of the ship
-//                throw new RuntimeException("amogus");
-            for (FighterLaunchBayAPI launchBay : ship.getLaunchBaysCopy()) {//gets launch bays of the ship
-                for (WeaponAPI wingWeapon : launchBay.getShip().getAllWeapons()) {//gets weapons on the leader wing from launch bay
-                    if (weapon.getSlot().isSystemSlot() || weapon.getSlot().isDecorative() || weapon.getSlot().isBuiltIn() || weapon.getSlot().isStationModule()) {
-                    } else {
-                        if (weapon.getSlot().getId().equals(wingWeapon.getSlot().getId())) {//check if any of the wings weapon slots ids are equal to any of the ships slot ids
-//                        for (ShipAPI wingMember : launchBay.getWing().getWingMembers()) {//gets the list of members in wing
-                            launchBay.getShip().getMutableStats().getVariant().addWeapon(weapon.getSlot().getId(), weapon.getId());//replaces the weapons of fighters
-//                        }
-                        }
-                    }
-                }
-            }
-        }
 
 
     }
 
     @Override
     public void advanceInCombat(ShipAPI ship, float amount) {
-        try {
-            if (ship != null) {
-                this.applyEffectsAfterShipCreation(ship, ship.getId());
-                for (FighterLaunchBayAPI launchBay : ship.getLaunchBaysCopy()) {
-                    launchBay.getWing().orderReturn(ship);
+
+//        try {
+
+
+            for (WeaponAPI weapon : ship.getAllWeapons()) {//gets weapons of the ship
+//                throw new RuntimeException("amogus");
+//                for (FighterLaunchBayAPI launchBay : ship.getLaunchBaysCopy()) {//gets launch bays of the ship //unused
+
+
+                for (FighterWingAPI wing : ship.getAllWings()) {//gets the wings
+                    for (WeaponAPI wingWeapon : wing.getLeader().getAllWeapons()) {//gets the weapon of the fighter
+
+                        for (WeaponAPI shipWeapon : ship.getAllWeapons()) {//gets the weapon of the ship
+
+                            if (shipWeapon.getSlot().getId().equals(wingWeapon.getSlot().getId())) {//checks if the ids are the same
+//                            wing.getLeader().getMutableStats().getVariant().clearSlot(wingWeapon.getId());//removes the weapon
+                                wing.getLeader().getMutableStats().getVariant().addWeapon(wingWeapon.getSlot().getId(), shipWeapon.getId());//adds the weapon
+
+                                wing.getLeader().getFleetMember().setVariant(wing.getLeader().getVariant(), false, true);
+                            }
+
+                        }
+
+                    }
+
                 }
-            }
-        } catch (Exception ex) {
-            return;
+
+//                }
+
         }
+        //  } catch (Exception ex) {
+        //          return;
+        //     }
+
+
+        //        try {
+//
+//            if (ship != null) {
+////                this.applyEffectsAfterShipCreation(ship, ship.getId());
+//                for (FighterLaunchBayAPI launchBay : ship.getLaunchBaysCopy()) {
+//
+//                    for (ShipAPI wing : launchBay.getWing().getWingMembers()) {
+//                        launchBay.land(wing);
+//                    }
+//
+//                }
+//            }
+//        } catch (Exception ex) {
+//            return;
+//        }
     }
 
 
